105502b1cf71a3f1bd0f600e3d4f7de58411650f
James Hennessy <james.ryan.hennessy@gmail.com>
2016-09-01 23:55:44 -0400

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -1,29 +1,7 @@
 #/bin/bash
-
-echo "Running Test 1"
-echo | python geneticmap.py 50 0.5 ;
-echo "Test 1 complete"
-echo "Running Test 2"
-echo | python geneticmap.py 50 1 ;
-echo "Test 2 complete"
-echo "Running Test 3"
-echo | python geneticmap.py 50 2 ;
-echo "Test 3 complete"
-echo "Running Test 4"
-echo | python geneticmap.py 100 0.5 ;
-echo "Test 4 complete"
-echo "Running Test 5"
-echo | python geneticmap.py 100 1 ;
-echo "Test 5 complete"
-echo "Running Test 6"
-echo | python geneticmap.py 100 2 ;
-echo "Test 6 complete"
-echo "Running Test 7"
-echo | python geneticmap.py 150 0.5 ;
-echo "Test 7 complete"
-echo "Running Test 8"
-echo | python geneticmap.py 150 1 ;
-echo "Test 8 complete"
-echo "Running Test 9"
-echo | python geneticmap.py 150 2 ;
-echo "Test 9 complete"
+for x in 1,2,3,4,5,6,7,8
+do
+echo "Running Test $x"
+echo | python geneticmap.py 50*$x 0.5*$x ;
+echo "Test $x complete"
+done
